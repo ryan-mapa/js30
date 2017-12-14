67bed19944f2ad71e71b8be532f410fe34d4d6c5
ryan mapa <ryan.m.mapa@gmail.com>
2017-12-13 18:18:45 -0800

    Fix skipButtons selector

--- video_player/scripts.js
+++ video_player/scripts.js
@@ -3,7 +3,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipbuttons = player.querySelectorAll('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
